e23d76b4085550a4c6f3fa4ee115500959e238a5
Kaz Bishop <EvilLogic@users.noreply.github.com>
2017-07-08 00:54:35 -0700

    Initial Commit

---
+++ BombDefuser/BombDefuser/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace BombDefuser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblMorse = new System.Windows.Forms.Label();
+            this.lblMHz = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Tag = "1";
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 51);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Tag = "2";
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDown);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 77);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Tag = "3";
+            this.textBox3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDown);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 103);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Tag = "4";
+            this.textBox4.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDown);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 129);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.Tag = "5";
+            this.textBox5.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDown);
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Location = new System.Drawing.Point(12, 152);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(45, 13);
+            this.lblPass.TabIndex = 5;
+            this.lblPass.Text = "Answer:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Password";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(118, 25);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 7;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(115, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Morse (0- )";
+            // 
+            // lblMorse
+            // 
+            this.lblMorse.AutoSize = true;
+            this.lblMorse.Location = new System.Drawing.Point(115, 48);
+            this.lblMorse.Name = "lblMorse";
+            this.lblMorse.Size = new System.Drawing.Size(45, 13);
+            this.lblMorse.TabIndex = 9;
+            this.lblMorse.Text = "Answer:";
+            // 
+            // lblMHz
+            // 
+            this.lblMHz.AutoSize = true;
+            this.lblMHz.Location = new System.Drawing.Point(115, 61);
+            this.lblMHz.Name = "lblMHz";
+            this.lblMHz.Size = new System.Drawing.Size(32, 13);
+            this.lblMHz.TabIndex = 10;
+            this.lblMHz.Text = "MHz:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 381);
+            this.Controls.Add(this.lblMHz);
+            this.Controls.Add(this.lblMorse);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Bomb_Defuser.exe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblMorse;
+        private System.Windows.Forms.Label lblMHz;
+    }
+}
+
---
+++ BombDefuser/BombDefuser/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace BombDefuser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.Equals(Keys.Enter))
+            {
+                if (((TextBox)sender).Text.Length != 6)
+                {
+                    MessageBox.Show("It needs to be 6 characters!");
+                    ((TextBox)sender).Focus();
+                    return;
+                }
+                switch (((TextBox)sender).Tag.ToString())
+                {
+                    case "1":
+                        textBox2.Focus();
+                        break;
+                    case "2":
+                        textBox3.Focus();
+                        break;
+                    case "3":
+                        textBox4.Focus();
+                        break;
+                    case "4":
+                        textBox5.Focus();
+                        break;
+                    case "5":
+                        decrypt();
+                        break;
+                }
+            }
+        }
+
+        private void decrypt()
+        {
+            String[] words = {"about", "after",	"again", "below", "could", 
+                                 "every", "first", "found", "great", "house", 
+                                 "large", "learn", "never", "other", "place",
+                                 "plant", "point", "right", "small", "sound",
+                                 "spell", "still", "study",	"their", "there",
+                                 "these", "thing", "think",	"three", "water",
+                                 "where", "which", "world",	"would", "write"};
+            Char[] one = textBox1.Text.ToCharArray();
+            //Char[] first = { 'a', 'b', 'c', 'e', 'f', 'g', 'h', 'l', 'n', 'o', 'p', 'r', 's', 't', 'w'};
+            Char[] two = textBox2.Text.ToCharArray();
+            //Char[] second = { 'a', 'b', 'g', 'e', 'o', 'v', 'i', };
+            Char[] three = textBox3.Text.ToCharArray();
+            Char[] four = textBox4.Text.ToCharArray();
+            Char[] five = textBox5.Text.ToCharArray();
+            for (int a = 0; a < 6; a++)
+                for (int b = 0; b < 6; b++)
+                    for (int c = 0; c < 6; c++)
+                        for (int d = 0; d < 6; d++)
+                            for (int e = 0; e < 6; e++)
+                                for (int f = 0; f < 35; f++)
+                                    if (charCombine(one[a], two[b], three[c], four[d], five[e]).Equals(words[f]))
+                                        lblPass.Text = (charCombine(one[a], two[b], three[c], four[d], five[e]));
+        }
+
+        private static string charCombine(char c0, char c1, char c2, char c3, char c4)
+        {
+            // Combine chars into array
+            char[] arr = new char[5];
+            arr[0] = c0;
+            arr[1] = c1;
+            arr[2] = c2;
+            arr[3] = c3;
+            arr[4] = c4;
+            // Return new string key
+            return new string(arr);
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            var words = new Dictionary<string, double>{
+                {"shell", 3.505}, {"halls", 3.515}, {"slick", 3.522},
+                {"trick", 3.532}, {"boxes", 3.535}, {"leaks", 3.542},
+                {"strobe", 3.545}, {"bistro", 3.552}, {"flick", 3.555},
+                {"bombs", 3.565}, {"break", 3.572}, {"brick", 3.575},
+                {"steak", 3.582}, {"sting", 3.592}, {"vector", 3.595},
+                {"beats", 3.600}
+            };
+            var morse = new Dictionary<string, char>{
+                {"0-", 'a'}, {"-000", 'b'}, {"-0-0", 'c'}, {"-00", 'd'},
+                {"0", 'e'}, {"00-0", 'f'}, {"--0", 'g'}, {"0000", 'h'},
+                {"00", 'i'}, {"0---", 'j'}, {"-0-", 'k'}, {"0-00", 'l'},
+                {"--", 'm'}, {"-0", 'n'}, {"---", 'o'}, {"0--0", 'p'},
+                {"--0-", 'q'}, {"0-0", 'r'}, {"000", 's'}, {"-", 't'},
+                {"00-", 'u'}, {"000-", 'v'}, {"0--", 'w'}, {"-00-", 'x'},
+                {"-0--", 'y'}, {"--00", 'z'}, {"0----", '1'}, {"00---", '2'},
+                {"000--", '3'}, {"0000-", '4'}, {"00000", '5'}, {"-0000", '6'},
+                {"--000", '7'}, {"---00", '8'}, {"----0", '9'}, {"-----", '0'}
+            };
+            String[] split = textBox6.Text.Split();
+            String answer = "";
+            try
+            {
+                foreach (string i in split)
+                    answer += morse[i];
+                lblMorse.Text = answer;
+                lblMHz.Text = words[answer].ToString();
+            }
+            catch (Exception ex) { }
+        }
+    }
+}
